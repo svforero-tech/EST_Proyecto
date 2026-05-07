b08f66c590b50ab2472ad3e9b3061699854dbfca
svforero-tech <svforero@uniboyaca.edu.co>
2026-05-07 12:26:55 -0500

    Forms2

---
+++ EST_Proyecto/Forms/Form2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Reflection;
+using System.Text;
+using System.Windows.Forms;
+
+namespace EST_Proyecto.Forms
+{
+    public partial class Form2 : Form
+    {
+
+        private void SetDoubleBuffered(Panel panel)
+        {
+            typeof(Panel).InvokeMember("DoubleBuffered",
+            BindingFlags.SetProperty |
+            BindingFlags.Instance |
+            BindingFlags.NonPublic,
+            null, panel, new object[] { true });
+        }
+        // Llamarlo en el constructor, tras InitializeComponent()
+        SetDoubleBuffered(panelTree);
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+    }
+}
